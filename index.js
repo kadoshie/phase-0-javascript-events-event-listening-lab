b649cf450d91c7cc8d0e6e0cf5b1aa703cff91d3
Moringa School <ruthmutiso69@gmail.com>
2022-09-20 17:58:00 +0300

    Done the Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked a while ago!');
+    });}
+    console.log('alert')
+    var input = document.getElementById('button')
+function clickAlert() {
+    alert('Iwas clicked a while ago!');
 }
+input.addEventListener('click', clickAlert)
